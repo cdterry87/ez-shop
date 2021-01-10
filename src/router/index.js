30e3908b95a848145728d3993dfced79b8f55817
Chase Terry <chase.terry87@gmail.com>
2021-01-09 23:57:28 -0600

    Setup the pages and components structure for the shop

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,95 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-Vue.use(VueRouter);
+import Home from '../pages/Home.vue'
+
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/about',
+    name: 'About',
+    component: () => import('../pages/About.vue')
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: () => import('../pages/Cart.vue')
+  },
+  {
+    path: '/checkout',
+    name: 'Checkout',
+    component: () => import('../pages/Checkout.vue')
+  },
+  {
+    path: '/checkout/:orderId',
+    name: 'CheckoutSuccess',
+    component: () => import('../pages/CheckoutSuccess.vue'),
+    props: true
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: () => import('../pages/Contact.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../pages/Login.vue')
+  },
+  {
+    path: '/orders',
+    name: 'Orders',
+    component: () => import('../pages/Orders.vue')
+  },
+  {
+    path: '/orders/:id',
+    name: 'Order',
+    component: () => import('../pages/Order.vue'),
+    props: true
+  },
+  {
+    path: '/privacy',
+    name: 'Privacy',
+    component: () => import('../pages/Privacy.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('../pages/Register.vue')
+  },
+  {
+    path: '/shop',
+    name: 'Shop',
+    component: () => import('../pages/Shop.vue')
+  },
+  {
+    path: '/shop/:category',
+    name: 'Category',
+    component: () => import('../pages/ShopCategory.vue'),
+    props: true
+  },
+  {
+    path: '/shop/:category/:product',
+    name: 'Product',
+    component: () => import('../pages/ShopProduct.vue')
+  },
+  {
+    path: '*',
+    name: 'NotFound',
+    component: () => import('../pages/NotFound.vue')
   }
-];
+]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes
-});
+})
 
-export default router;
+export default router
